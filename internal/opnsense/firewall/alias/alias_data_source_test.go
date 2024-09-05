548bc5469a69eeb61a24dad7fd7bdafed7113014
Ryan Ng <ryan.ng.dev@tinynamoo.com>
2024-09-06 00:43:25 +0800

    test: add alias datasource via name test case

--- internal/opnsense/firewall/alias/alias_data_source_test.go
+++ internal/opnsense/firewall/alias/alias_data_source_test.go
@@ -16,9 +16,30 @@ func TestAccAliasDataSource_host(t *testing.T) {
 		PreCheck:                 func() { acctest.TestAccPreCheck(t) },
 		ProtoV6ProviderFactories: acctest.TestAccProtoV6ProviderFactories,
 		Steps: []resource.TestStep{
-			// Read testing
+			// Read testing (via id)
 			{
-				Config: acctest.ProviderConfig + testAccAliasHostDataSourceConfig,
+				Config: acctest.ProviderConfig + testAccAliasHostDataSourceIdConfig,
+				ConfigStateChecks: []statecheck.StateCheck{
+					statecheck.ExpectKnownValue("data.opnsense_firewall_alias.test_acc_data_source_host", tfjsonpath.New("enabled"), knownvalue.Bool(true)),
+					statecheck.ExpectKnownValue("data.opnsense_firewall_alias.test_acc_data_source_host", tfjsonpath.New("name"), knownvalue.StringExact("test_acc_alias_host_data_source")),
+					statecheck.ExpectKnownValue("data.opnsense_firewall_alias.test_acc_data_source_host", tfjsonpath.New("type"), knownvalue.StringExact("host")),
+					statecheck.ExpectKnownValue("data.opnsense_firewall_alias.test_acc_data_source_host", tfjsonpath.New("counters"), knownvalue.Bool(true)),
+					statecheck.ExpectKnownValue("data.opnsense_firewall_alias.test_acc_data_source_host", tfjsonpath.New("updatefreq"), knownvalue.ObjectExact(map[string]knownvalue.Check{
+						"days":  knownvalue.Int32Exact(0),
+						"hours": knownvalue.Float64Exact(0),
+					})),
+					statecheck.ExpectKnownValue("data.opnsense_firewall_alias.test_acc_data_source_host", tfjsonpath.New("description"), knownvalue.StringExact("host alias for terraform datasource testing")),
+					statecheck.ExpectKnownValue("data.opnsense_firewall_alias.test_acc_data_source_host", tfjsonpath.New("proto"), knownvalue.ObjectExact(map[string]knownvalue.Check{
+						"ipv4": knownvalue.Bool(false),
+						"ipv6": knownvalue.Bool(false),
+					})),
+					statecheck.ExpectKnownValue("data.opnsense_firewall_alias.test_acc_data_source_host", tfjsonpath.New("content"), knownvalue.ListExact([]knownvalue.Check{knownvalue.StringExact("1.1.1.1")})),
+					statecheck.ExpectKnownValue("data.opnsense_firewall_alias.test_acc_data_source_host", tfjsonpath.New("interfaces"), knownvalue.Null()),
+				},
+			},
+			// Read testing (via name)
+			{
+				Config: acctest.ProviderConfig + testAccAliasHostDataSourceNameConfig,
 				ConfigStateChecks: []statecheck.StateCheck{
 					statecheck.ExpectKnownValue("data.opnsense_firewall_alias.test_acc_data_source_host", tfjsonpath.New("enabled"), knownvalue.Bool(true)),
 					statecheck.ExpectKnownValue("data.opnsense_firewall_alias.test_acc_data_source_host", tfjsonpath.New("name"), knownvalue.StringExact("test_acc_alias_host_data_source")),
@@ -41,8 +62,8 @@ func TestAccAliasDataSource_host(t *testing.T) {
 	})
 }
 
-// testAccAliasDataSourceConfig creates an alias resource and imports it as a data source
-const testAccAliasHostDataSourceConfig = `
+// testAccAliasHostDataSourceIdConfig creates an alias resource and imports it as a data source via its id
+const testAccAliasHostDataSourceIdConfig = `
 	resource "opnsense_firewall_alias" "test_acc_data_source_host" {
 		enabled = true
 		name = "test_acc_alias_host_data_source"
@@ -56,6 +77,28 @@ const testAccAliasHostDataSourceConfig = `
 
 	data "opnsense_firewall_alias" "test_acc_data_source_host" {
 		id = opnsense_firewall_alias.test_acc_data_source_host.id
+	}
+
+	output "test_acc_alias_host" {
+		value = data.opnsense_firewall_alias.test_acc_data_source_host
+	}
+`
+
+// testAccAliasHostDataSourceNameConfig creates an alias resource and imports it as a data source via its name
+const testAccAliasHostDataSourceNameConfig = `
+	resource "opnsense_firewall_alias" "test_acc_data_source_host" {
+		enabled = true
+		name = "test_acc_alias_host_data_source"
+		type = "host"
+		counters = true
+		description = "host alias for terraform datasource testing"
+		content = [
+			"1.1.1.1"
+		]
+	}
+
+	data "opnsense_firewall_alias" "test_acc_data_source_host" {
+		name = opnsense_firewall_alias.test_acc_data_source_host.name
 	}
 
 	output "test_acc_alias_host" {
