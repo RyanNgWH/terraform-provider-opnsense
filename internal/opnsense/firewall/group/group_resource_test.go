7e1d078d799feb61d34cc4963123381ecc9bc528
Ryan Ng <ryan.ng.dev@tinynamoo.com>
2024-09-13 23:29:11 +0800

    test: add firewall group resource test cases

---
+++ internal/opnsense/firewall/group/group_resource_test.go
@@ -0,0 +1,84 @@
+package group_test
+
+import (
+	"testing"
+
+	"terraform-provider-opnsense/internal/acctest"
+
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/hashicorp/terraform-plugin-testing/knownvalue"
+	"github.com/hashicorp/terraform-plugin-testing/statecheck"
+	"github.com/hashicorp/terraform-plugin-testing/tfjsonpath"
+)
+
+func TestAccGroupResource(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		PreCheck:                 func() { acctest.TestAccPreCheck(t) },
+		ProtoV6ProviderFactories: acctest.TestAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			// Create and Read testing
+			{
+				Config: testAccGroupResourceConfig,
+				ConfigStateChecks: []statecheck.StateCheck{
+					statecheck.ExpectKnownValue("opnsense_firewall_group.test_acc_resource", tfjsonpath.New("name"), knownvalue.StringExact("test_acc_group")),
+					statecheck.ExpectKnownValue("opnsense_firewall_group.test_acc_resource", tfjsonpath.New("members"), knownvalue.ListExact([]knownvalue.Check{
+						knownvalue.StringExact("lan"),
+						knownvalue.StringExact("wan"),
+					})),
+					statecheck.ExpectKnownValue("opnsense_firewall_group.test_acc_resource", tfjsonpath.New("no_group"), knownvalue.Bool(true)),
+					statecheck.ExpectKnownValue("opnsense_firewall_group.test_acc_resource", tfjsonpath.New("sequence"), knownvalue.Int64Exact(100)),
+					statecheck.ExpectKnownValue("opnsense_firewall_group.test_acc_resource", tfjsonpath.New("description"), knownvalue.StringExact("firewall group for terraform resource testing")),
+				},
+			},
+			// ImportState testing
+			{
+				ResourceName:      "opnsense_firewall_group.test_acc_resource",
+				ImportState:       true,
+				ImportStateId:     "test_acc_group",
+				ImportStateVerify: true,
+				// The last_updated attribute does not exist in the HashiCups
+				// API, therefore there is no value for it during import.
+				ImportStateVerifyIgnore: []string{"last_updated"},
+			},
+			// Update and Read testing
+			{
+				Config: testAccGroupDefaultResourceConfig,
+				ConfigStateChecks: []statecheck.StateCheck{
+					statecheck.ExpectKnownValue("opnsense_firewall_group.test_acc_resource", tfjsonpath.New("name"), knownvalue.StringExact("test_acc_ugroup")),
+					statecheck.ExpectKnownValue("opnsense_firewall_group.test_acc_resource", tfjsonpath.New("members"), knownvalue.ListExact([]knownvalue.Check{
+						knownvalue.StringExact("lan"),
+					})),
+					statecheck.ExpectKnownValue("opnsense_firewall_group.test_acc_resource", tfjsonpath.New("no_group"), knownvalue.Bool(false)),
+					statecheck.ExpectKnownValue("opnsense_firewall_group.test_acc_resource", tfjsonpath.New("sequence"), knownvalue.Int64Exact(0)),
+					statecheck.ExpectKnownValue("opnsense_firewall_group.test_acc_resource", tfjsonpath.New("description"), knownvalue.StringExact("")),
+				},
+			},
+			// Delete testing automatically occurs in TestCase
+		},
+	})
+}
+
+// testAccGroupResourceConfig defines a group resource.
+const testAccGroupResourceConfig = `
+	resource "opnsense_firewall_group" "test_acc_resource" {
+		name = "test_acc_group"
+		members = [
+			"lan",
+			"wan"
+		]
+
+		no_group    = true
+		sequence    = 100
+		description = "firewall group for terraform resource testing"
+	}
+`
+
+// testAccGroupDefaultResourceConfig defines a group resource with only required fields.
+const testAccGroupDefaultResourceConfig = `
+	resource "opnsense_firewall_group" "test_acc_resource" {
+		name = "test_acc_ugroup"
+		members = [
+			"lan",
+		]
+	}
+`
