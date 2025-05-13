4092a30fce55eef910eedbb2176abf131888f68b
Ryan Ng <ryan.ng.dev@tinynamoo.com>
2025-05-13 23:07:35 +0800

    docs(one-to-one nat): add example configurations & documentation

---
+++ examples/resources/opnsense_firewall_nat_one_to_one/import.sh
@@ -0,0 +1,2 @@
+# One-to-one NAT rules can be imported by specifying the rule UUID. This can be found by examining the API calls on the OPNsense web interface
+terraform import opnsense_firewall_nat_one_to_one.import_example 7ce41c23-968c-46f2-9b6b-3c9a1519086f
---
+++ examples/resources/opnsense_firewall_nat_one_to_one/resource.tf
@@ -0,0 +1,15 @@
+# Example one-to-one NAT rule mapping
+resource "opnsense_firewall_nat_one_to_one" "resource_example" {
+  enabled         = true
+  log             = true
+  sequence        = 2
+  interface       = "opt1"
+  type            = "binat"
+  source_net      = "10.2.72.1/32"
+  source_not      = false
+  destination_net = "any"
+  destination_not = false
+  external        = "10.2.0.1"
+  nat_reflection  = "default"
+  description     = "Example one-to-one NAT mapping"
+}
