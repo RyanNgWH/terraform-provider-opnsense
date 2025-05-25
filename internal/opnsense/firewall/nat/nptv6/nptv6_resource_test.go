9f591fa8d91a962926d1e8a9a1e6bf6d1a97d140
Ryan Ng <ryan.ng.dev@tinynamoo.com>
2025-05-26 01:24:19 +0800

    test(nptv6): add nptv6 resource default values test case

--- internal/opnsense/firewall/nat/nptv6/nptv6_resource_test.go
+++ internal/opnsense/firewall/nat/nptv6/nptv6_resource_test.go
@@ -65,6 +65,37 @@ func TestAccOneToOneNatResource_nat(t *testing.T) {
 	})
 }
 
+func TestAccOneToOneNatResource_default(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		PreCheck:                 func() { acctest.TestAccPreCheck(t) },
+		ProtoV6ProviderFactories: acctest.TestAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			// Create and Read testing
+			{
+				Config: testAccNptv6NatResourceConfig_default,
+				ConfigStateChecks: []statecheck.StateCheck{
+					statecheck.ExpectKnownValue("opnsense_firewall_nat_nptv6.test_acc_resource_nptv6", tfjsonpath.New("enabled"), knownvalue.Bool(true)),
+					statecheck.ExpectKnownValue("opnsense_firewall_nat_nptv6.test_acc_resource_nptv6", tfjsonpath.New("log"), knownvalue.Bool(false)),
+					statecheck.ExpectKnownValue("opnsense_firewall_nat_nptv6.test_acc_resource_nptv6", tfjsonpath.New("sequence"), knownvalue.Int32Exact(1)),
+					statecheck.ExpectKnownValue("opnsense_firewall_nat_nptv6.test_acc_resource_nptv6", tfjsonpath.New("interface"), knownvalue.StringExact("wan")),
+					statecheck.ExpectKnownValue("opnsense_firewall_nat_nptv6.test_acc_resource_nptv6", tfjsonpath.New("internal_prefix"), knownvalue.StringExact("1::")),
+					statecheck.ExpectKnownValue("opnsense_firewall_nat_nptv6.test_acc_resource_nptv6", tfjsonpath.New("track_interface"), knownvalue.StringExact("")),
+					statecheck.ExpectKnownValue("opnsense_firewall_nat_nptv6.test_acc_resource_nptv6", tfjsonpath.New("categories"), knownvalue.ListExact([]knownvalue.Check{})),
+					statecheck.ExpectKnownValue("opnsense_firewall_nat_nptv6.test_acc_resource_nptv6", tfjsonpath.New("description"), knownvalue.StringExact("")),
+				},
+			},
+			// ImportState testing
+			{
+				ResourceName:            "opnsense_firewall_nat_nptv6.test_acc_resource_nptv6",
+				ImportState:             true,
+				ImportStateVerify:       true,
+				ImportStateVerifyIgnore: []string{"last_updated"},
+			},
+			// Delete testing automatically occurs in TestCase
+		},
+	})
+}
+
 // testAccNptv6NatResourceConfig defines a NPTv6 NAT resource.
 const testAccNptv6NatResourceConfig = `
 	resource "opnsense_firewall_nat_nptv6" "test_acc_resource_nptv6" {
@@ -92,5 +123,13 @@ const testAccNptv6NatResourceConfig_modified = `
 		track_interface = "lan"
 		categories = []
 		description = "[Updated] NPTv6 nat rule for terraform resource testing"
+	}
+`
+
+// testAccNptv6NatResourceConfig_default defines a NPTv6 NAT resource with default values.
+const testAccNptv6NatResourceConfig_default = `
+	resource "opnsense_firewall_nat_nptv6" "test_acc_resource_nptv6" {
+		interface       = "wan"
+		internal_prefix = "1::"
 	}
 `
