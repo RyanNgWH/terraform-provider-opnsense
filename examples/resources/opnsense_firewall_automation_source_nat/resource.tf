3ee6c34e710f8f879dde278f264997c00ad4cc7b
Ryan Ng <ryan.ng.dev@tinynamoo.com>
2025-05-30 16:23:01 +0800

    docs(automation_source_nat): add example configurations

---
+++ examples/resources/opnsense_firewall_automation_source_nat/resource.tf
@@ -0,0 +1,22 @@
+# Example firewall automation source nat rule
+resource "opnsense_firewall_automation_filter" "test_acc_resource_filter" {
+		enabled     	   = true
+		no_nat 		  	   = false
+		sequence    	   = 2
+		interface   	   = "wan"
+		ip_version  	   = "ipv4"
+		protocol 	  	   = "tcp"
+		source 		  	   = "1.1.1.1"
+		source_not  	   = false
+		source_port 	   = "ssh"
+		destination 	   = "2.2.2.2"
+		destination_not  = false
+		destination_port = "ssh"
+		target 					 = "3.3.3.3"
+		target_port 		 = "ssh"
+		log 						 = false
+		categories 			 = [
+			"source_nat"
+		]
+  description = "Example firewall automation source nat rule"
+}
