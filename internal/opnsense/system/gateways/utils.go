d36363b031595f1d594bac8a9adc28c1da178059
Ryan Ng <ryan.ng.dev@tinynamoo.com>
2025-05-31 15:27:33 +0800

    refactor(gateway): migrate to common name in messages

--- internal/opnsense/system/gateways/utils.go
+++ internal/opnsense/system/gateways/utils.go
@@ -6,11 +6,15 @@ import (
 	"terraform-provider-opnsense/internal/opnsense"
 )
 
+const (
+	resourceName string = "gateway"
+)
+
 // VerifyGateway checks if the specified gateway exist on the OPNsense firewall.
 func VerifyGateway(client *opnsense.Client, gateway string) (bool, error) {
 	uuid, err := searchGateway(client, gateway)
 	if err != nil {
-		return false, fmt.Errorf("Verify gateway exists error: %s", err)
+		return false, fmt.Errorf("Verify %s exists error: %s", resourceName, err)
 	}
 
 	if uuid == "" {
